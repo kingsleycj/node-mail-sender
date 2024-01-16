6362706f22768c2a0fe96ca02cbde62a58cdf1f7
kingsleycj <primekings.kc@gmail.com>
2024-01-16 23:07:58 +0100

    formatting

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config({ path: './config.env' });
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: "./config.env" });
 }
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const nodemailer = require('nodemailer');
-const sgMail = require('@sendgrid/mail');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.NODE_SENDGRID_KEY);
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 // view engine setup
-app.set('view engine', 'ejs');
-app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set("view engine", "ejs");
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-	res.render('contact');
+app.get("/", (req, res) => {
+  res.render("contact");
 });
 
-app.post('/send', async (req, res) => {
-	const output = `
+app.post("/send", async (req, res) => {
+  const output = `
     <p>You have a new contact request</p>
     <h3>Contact details</h3>
     <ul>
@@ -33,45 +33,45 @@ app.post('/send', async (req, res) => {
 		<li>Message: ${req.body.message} </li>
     </ul>
 	`;
-	const choice = Math.ceil(Math.random() * 2);
-	if (choice === 1) {
-		const msg = {
-			to: process.env.NODE_MAIL_RECEIVER,
-			from: `SendGrid Contact <${process.env.NODE_MAIL_SENDGRID_SENDER_MAIL}>`,
-			subject: 'Contact Request via Sendgrid',
-			text: 'Mail is sent by Send Grid',
-			html: output,
-		};
-		try {
-			await sgMail.send(msg);
-			res.redirect('/');
-		} catch (e) {
-			console.log(e);
-		}
-	} else {
-		let transporter = nodemailer.createTransport({
-			service: 'gmail',
-			auth: {
-				user: process.env.NODE_SENDER_MAIL,
-				pass: process.env.NODE_SENDER_MAIL_PASSWORD,
-			},
-		});
-		let mailOptions = {
-			from: `Nodemailer Contact <${process.env.NODE_SENDER_MAIL}>`,
-			to: process.env.NODE_MAIL_RECEIVER,
-			subject: 'Contact Request via Nodemailer',
-			html: output,
-		};
-		transporter.sendMail(mailOptions, function (error, info) {
-			if (error) {
-				return console.log(error);
-			}
-			console.log('Message sent: %s', info.messageId);
-			res.redirect('/');
-		});
-	}
+  const choice = Math.ceil(Math.random() * 2);
+  if (choice === 1) {
+    const msg = {
+      to: process.env.NODE_MAIL_RECEIVER,
+      from: `SendGrid Contact <${process.env.NODE_MAIL_SENDGRID_SENDER_MAIL}>`,
+      subject: "Contact Request via Sendgrid",
+      text: "Mail is sent by Send Grid",
+      html: output,
+    };
+    try {
+      await sgMail.send(msg);
+      res.redirect("/");
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.NODE_SENDER_MAIL,
+        pass: process.env.NODE_SENDER_MAIL_PASSWORD,
+      },
+    });
+    let mailOptions = {
+      from: `Nodemailer Contact <${process.env.NODE_SENDER_MAIL}>`,
+      to: process.env.NODE_MAIL_RECEIVER,
+      subject: "Contact Request via Nodemailer",
+      html: output,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("Message sent: %s", info.messageId);
+      res.redirect("/");
+    });
+  }
 });
 
 app.listen(PORT, () => {
-	console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
